a063937d5fdb070c9b38609a9f6cee86db416c13
Aleksandra Cichocka <54075858+Aleks512@users.noreply.github.com>
2023-04-18 23:33:39 +0200

    admin's setup

--- users/models.py
+++ users/models.py
@@ -13,7 +13,6 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 from django.contrib.auth.base_user import BaseUserManager
 from django.utils.translation import gettext_lazy as _
 
-
 class CustomAccountManager(BaseUserManager):
     """
     Custom user model manager where email is the unique identifiers
@@ -35,26 +34,35 @@ class CustomAccountManager(BaseUserManager):
         """
         Create and save a SuperUser with the given email and password.
         """
+        extra_fields.setdefault("user_name", "Administrateur")
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
+        extra_fields.setdefault("company", "Ventalis")
 
         if extra_fields.get("is_staff") is not True:
             raise ValueError(_("Superuser must have is_staff=True."))
         if extra_fields.get("is_superuser") is not True:
             raise ValueError(_("Superuser must have is_superuser=True."))
+
         return self.create_user(email, password, **extra_fields)
 
 class NewUser(AbstractBaseUser, PermissionsMixin):
-
+    user_name = models.CharField(max_length=150, unique=True)
     email = models.EmailField(_('Email'),unique=True, blank=False, max_length=255)
-    is_staff = models.BooleanField(default=False)
+    first_name = models.CharField(_("Prénom"), max_length=100)
+    last_name = models.CharField(_("Nom de famille"), max_length=50)
+    company = models.CharField(_("Société"), max_length=100)
     is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_client = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     date_joined = models.DateTimeField(default=timezone.now)
-
 
     objects = CustomAccountManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+    def __str__(self):
+        return self.user_name
 
--- users/tests.py
+++ users/tests.py
@@ -12,6 +12,7 @@ class UserAccountTests(TestCase):
         self.assertTrue(super_user.is_superuser)
         self.assertTrue(super_user.is_staff)
         self.assertTrue(super_user.is_active)
+        self.assertEqual(str(super_user), "Administrateur")
 
         with self.assertRaises(ValueError):
             db.objects.create_superuser(
@@ -25,15 +26,4 @@ class UserAccountTests(TestCase):
             db.objects.create_superuser(
                 email='', password='password', is_superuser=True)
 
-    def test_new_user(self):
-        db = get_user_model()
-        user = db.objects.create_user(
-            'testuser@user.com', 'password')
-        self.assertEqual(user.email, 'testuser@user.com')
-        self.assertFalse(user.is_superuser)
-        self.assertFalse(user.is_staff)
-        self.assertFalse(user.is_active)
 
-        with self.assertRaises(ValueError):
-            db.objects.create_user(
-                email='', password='password')
