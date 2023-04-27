cc0a1eff8f51d857d165e585ce959af3113f48c8
Aleksandra Cichocka <54075858+Aleks512@users.noreply.github.com>
2023-04-27 23:19:20 +0200

    admins_private_access

--- users/views.py
+++ users/views.py
@@ -8,6 +8,7 @@ from .forms import ConsultantCreationForm, CustomerCreationForm
 from .models import Consultant, Customer
 from django.db import models
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth.mixins import UserPassesTestMixin
 
 
 
@@ -15,25 +16,33 @@ def home(request):
 
     return render(request, "users/home.html")
 
-class ConsultantCreateView(CreateView):
+class ConsultantCreateView(UserPassesTestMixin,CreateView):
     template_name = 'users/consultant_create.html'
     form_class = ConsultantCreationForm
     success_url = reverse_lazy('consultant-list')
     model = Consultant
-
-class ConsultantListView(ListView):
+    def test_func(self):
+        # Vérifier si l'utilisateur en cours est authentifié et superutilisateur
+        return self.request.user.is_authenticated and self.request.user.is_superuser
+class ConsultantListView(UserPassesTestMixin, ListView):
     model = Consultant
     template_name = 'user/consultant_list.html'
+    def test_func(self):
+        # Vérifier si l'utilisateur en cours est authentifié et superutilisateur
+        return self.request.user.is_authenticated and self.request.user.is_superuser
 
 class SignUp(generic.CreateView):
     form_class = CustomerCreationForm
     success_url = reverse_lazy("customers-list")
     template_name = "users/client_signup.html"
 
-class CustomerListView(ListView):
+class CustomerListView(UserPassesTestMixin, ListView):
     model = Customer
     template_name = 'users/customers_list.html'
     context_object_name = 'customers'
+    def test_func(self):
+        # Vérifier si l'utilisateur en cours est authentifié et superutilisateur
+        return self.request.user.is_authenticated and self.request.user.is_superuser
 
     def get_queryset(self):
         return Customer.objects.select_related('consultant_applied')
@@ -44,16 +53,19 @@ class CustomerListView(ListView):
         return context
 
 
-class ConsultantListView(ListView):
+class ConsultantListView(UserPassesTestMixin, ListView):
     model = Consultant
     template_name = 'users/consultant_list.html'
+    def test_func(self):
+        # Vérifier si l'utilisateur en cours est authentifié et superutilisateur
+        return self.request.user.is_authenticated and self.request.user.is_superuser
 
     def get_queryset(self):
         queryset = super().get_queryset()
         queryset = queryset.annotate(num_clients=models.Count('clients'))
         return queryset
 
-class ConsultantUpdate(UpdateView):
+class ConsultantUpdate(UserPassesTestMixin, UpdateView):
     model = Consultant
     template_name = "users/consultant_update_form.html"
     fields = ['email', 'password', 'first_name', 'last_name', 'company']
@@ -63,13 +75,20 @@ class ConsultantUpdate(UpdateView):
     def form_valid(self, form):
         form.instance.password = make_password(form.cleaned_data['password'])
         return super().form_valid(form)
+    def test_func(self):
+        # Vérifier si l'utilisateur en cours est authentifié et superutilisateur
+        return self.request.user.is_authenticated and self.request.user.is_superuser
 
-class ConsultantDelete(DeleteView):
+class ConsultantDelete(UserPassesTestMixin, DeleteView):
         model = Consultant
         template_name = "users/consultant_delete_form.html"
         fields = '__all__'
         def get_success_url(self):
             return '/consultant/list'
+
+        def test_func(self):
+            # Vérifier si l'utilisateur en cours est authentifié et superutilisateur
+            return self.request.user.is_authenticated and self.request.user.is_superuser
 
 class AdminHomePageView(TemplateView):
     template_name = "users/home_admin.html"
