c55e5e10599e868d28692af858f77ceb73d915eb
Aleksandra Cichocka <54075858+Aleks512@users.noreply.github.com>
2023-04-28 08:41:17 +0200

    Tests_admins_lists_views

--- users/tests.py
+++ users/tests.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import User
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
-from .models import Consultant, Customer
+from .models import Consultant, Customer, NewUser
+from django.urls import reverse
+from django.contrib.auth.models import Permission
 
 
 class UserAccountTests(TestCase):
@@ -125,5 +127,48 @@ class ConsultantModelTest(TestCase):
 
             self.assertEqual(consultant, None)
 
+class CustomerListViewTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.client = Client()
+        cls.user = get_user_model().objects.create_superuser(
+            email='testuser1@example.com',
+            password='testpass'
+        )
+        cls.customer1 = Customer.objects.create(
+            consultant_applied=None,
+        )
+
+    def test_customer_list_view(self):
+        self.client.login(email=self.user.email, password='testpass')
+        response = self.client.get(reverse('customers-list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/customers_list.html')
+        self.assertContains(response, 'Liste de clients')
 
 
+
+
+class ConsultantListViewTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.client = Client()
+        cls.user = get_user_model().objects.create_superuser(
+            email='testuser@example.com',
+            password='testpass'
+        )
+        cls.permission = Permission.objects.get(name='Can view consultant')
+        cls.consultant = Consultant.objects.create(
+            matricule='ABCD',
+            email='consultant1@example.com'
+        )
+        cls.consultant.user_permissions.add(cls.permission)
+
+    def test_consultant_list_view(self):
+        self.client.login(email=self.user.email, password='testpass')
+        response = self.client.get(reverse('consultant-list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/consultant_list.html')
+        self.assertContains(response, self.consultant.matricule)
+        self.assertContains(response, self.consultant.email)
+        self.assertContains(response, self.consultant.get_clients_count())
