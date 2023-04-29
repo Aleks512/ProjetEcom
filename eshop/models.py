c0ff27a70e5118834f7ea5158c680502d7cac4e2
Aleksandra Cichocka <54075858+Aleks512@users.noreply.github.com>
2023-04-29 17:44:28 +0200

    category_slug_added

--- eshop/models.py
+++ eshop/models.py
@@ -1,14 +1,21 @@
 from decimal import Decimal
 
 from django.db import models
+from django.utils.text import slugify
+
 
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, blank=True)
 
     def __str__(self):
         return self.name
 
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
