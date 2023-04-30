73f0197d12041943830f3345e89018de0fee8543
Aleksandra Cichocka <54075858+Aleks512@users.noreply.github.com>
2023-04-30 11:57:13 +0200

    url_produits

--- eshop/urls.py
+++ eshop/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
 
-    path('',views.products, name="products"),
+    path('produits/',views.products, name="products"),
     path('produit/<str:slug>/',views.product_detail, name="product"),
 ]
