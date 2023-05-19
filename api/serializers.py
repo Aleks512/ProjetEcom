f32ea3bc76586a80fad60094bc61b708b1ef3361
Aleksandra Cichocka <54075858+Aleks512@users.noreply.github.com>
2023-05-19 20:09:43 +0200

    first_serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,1 +1,30 @@
-{\rtf1}
+from rest_framework import serializers
+from users.models import Customer
+from eshop.models import Order, Comment, Product
+from contact.models import Message
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = ['id', 'sender', 'recipient', 'content', 'timestamp']
+        read_only_fields = ['id', 'sender', 'timestamp']
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['name', 'image', 'description', 'unit_price', 'stock']
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = '__all__'
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
