9f594bc44fd0422d630ff4fa614c5503a8b1d1e9
Aleksandra Cichocka <54075858+Aleks512@users.noreply.github.com>
2023-05-23 05:55:48 +0200

    bs

--- ventalis/settings.py
+++ ventalis/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'eshop',
-    'django-bootstrap5',
+    'bootstrap5',
     'contact',
     'rest_framework',
     'rest_framework_simplejwt',
